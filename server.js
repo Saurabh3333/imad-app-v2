515e79049335bd8eab546503fb1fe3a69bd34ec6
Saurabh Shubham <saurabh.friday@gmail.com>
2017-02-14 19:39:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/article-one', function (req, res) {
-  res.send(reateTemplate(articleOne));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/ui/madi.png', function (req, res) {
