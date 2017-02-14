7a1f175c1e39ed5b59eed7bec45c4e33bea65006
Saurabh Shubham <saurabh.friday@gmail.com>
2017-02-14 19:41:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ function createTemplate(data) {
   var heading = data.heading;
   var content = data.content;
   
-  var htmltemplete = `
+  var htmlTemplete = `
 <html>
 <head>
     <title>${title}</title>
